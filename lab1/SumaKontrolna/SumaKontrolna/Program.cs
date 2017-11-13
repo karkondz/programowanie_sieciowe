fc03aa14c9ff70bf3153acfa0551d1db4700723c
kakrondz <karolina.kondzior@gmail.com>
2017-11-13 19:31:29 +0100

    SumaKontrolna_lab1

--- lab1/SumaKontrolna/SumaKontrolna/Program.cs
+++ lab1/SumaKontrolna/SumaKontrolna/Program.cs
@@ -12,19 +12,19 @@ namespace ConsoleApplication
             byte b=0;
             byte a = 0;
             byte[] tab = new byte[] {0, 1, 1, 0, 1, 0, 1, 0, 0};
-           // char[] tab2 = new char[] {'a', 'b'};
+            char[] tab2 = new char[] {'a', 'c'};
 
            // Console.WriteLine(nowa);
 
           //  for (int i = 0; i < 1; i++)
            // {
-                foreach(var v in tab){
+                foreach(var v in tab2){
 
                 for (int j = 1; j < 8; j++)
                 {
                     m = (byte)(1 << (j-1));
 
-                    b=(byte)(Convert.ToByte(Convert.ToBoolean(v) & Convert.ToBoolean(m))>> (j-1));
+                    b=(byte)(v & m>> (j-1));
                     //b=nowa & m;
 
                     a ^= b;
